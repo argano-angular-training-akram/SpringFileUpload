39371a927265a3a82ef5ed8cb1b88d89de49bb42
Akram Mohammad <akram.mohammad@argano.com>
2022-01-31 16:35:17 +0530

    Intital Commit

---
+++ src/main/java/com/journaldev/spring/controller/AmazonClient.java
@@ -0,0 +1,103 @@
+package com.journaldev.spring.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+@Service
+public class AmazonClient {
+	private AmazonS3Client s3client;
+
+//    @Value("${amazonProperties.endpointUrl}")
+//    private String endpointUrl;
+//    @Value("${amazonProperties.bucketName}")
+//    private String bucketName;
+//    @Value("${amazonProperties.accessKey}")
+//    private String accessKey;
+//    @Value("${amazonProperties.secretKey}")
+//    private String secretKey;
+    
+    private String endpointUrl = "https://s3.ap-south-1.amazonaws.com";
+    private String bucketName = "test-upload-argano";
+    private String accessKey = "AKIASTMTRDMNYJSHEH5E";
+    private String secretKey = "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";
+    
+    @PostConstruct
+    private void initializeAmazon() {
+       AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+       this.s3client = new AmazonS3Client(credentials);
+    }
+    
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
+    
+    private String generateFileName(MultipartFile multiPart) {
+        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
+    }
+    
+    private void uploadFileTos3bucket(String fileName, File file) {
+        s3client.putObject(new PutObjectRequest(bucketName, fileName, file));
+    }
+    
+    public String uploadFile(MultipartFile multipartFile) {
+
+        String fileUrl = "";
+        String fileName = "";
+        try {
+            File file = convertMultiPartToFile(multipartFile);
+            fileName = generateFileName(multipartFile);
+            fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
+            uploadFileTos3bucket(fileName, file);
+            file.delete();
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+        return fileName;
+//    	String filenameExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+//		
+//		String key = UUID.randomUUID().toString() + "." +filenameExtension;
+//		
+//		ObjectMetadata metaData = new ObjectMetadata();
+//		metaData.setContentLength(file.getSize());
+//		metaData.setContentType(file.getContentType());
+//		
+//		try {
+//			s3client.putObject("my-videos-bucket-05", key, file.getInputStream(), metaData);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		s3client.setObjectAcl("my-videos-bucket-05", key, CannedAccessControlList.PublicRead);
+//		
+//		return s3client.getResourceUrl("my-videos-bucket-05", key);
+    }
+    
+    public String deleteFileFromS3Bucket(String fileUrl) {
+        String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+        s3client.deleteObject(new DeleteObjectRequest(bucketName + "/", fileName));
+        return "Successfully deleted";
+    }
+}
---
+++ src/main/java/com/journaldev/spring/controller/FileUploadController.java
@@ -0,0 +1,142 @@
+package com.journaldev.spring.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+
+
+/**
+ * Handles requests for the application file upload requests
+ */
+@Controller
+public class FileUploadController {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(FileUploadController.class);
+
+	/**
+	 * Upload single file using Spring Controller
+	 */
+	
+	@Autowired
+	private AmazonClient amazonClient;
+
+    @Autowired
+    FileUploadController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public @ResponseBody String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+    	String response = this.amazonClient.uploadFile(file);
+    	System.out.println("response >>"+response);
+    	return "File Succesfully uploaded to S3 with name >> "+response;
+    }
+
+    @DeleteMapping("/deleteFile")
+    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
+        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+    }
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(HttpServletRequest request) throws IOException {
+	    System.out.println(request.getContextPath());
+	    return "home";
+	}
+//	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+//	public @ResponseBody
+//	String uploadFileHandler(@RequestParam("name") String name,
+//			@RequestParam("file") MultipartFile file) {
+//
+//		if (!file.isEmpty()) {
+//			try {
+//				byte[] bytes = file.getBytes();
+//
+//				// Creating the directory to store file
+//				String rootPath = System.getProperty("catalina.home");
+//				File dir = new File(rootPath + File.separator + "tmpFiles");
+//				if (!dir.exists())
+//					dir.mkdirs();
+//
+//				// Create the file on server
+//				File serverFile = new File(dir.getAbsolutePath()
+//						+ File.separator + name);
+//				BufferedOutputStream stream = new BufferedOutputStream(
+//						new FileOutputStream(serverFile));
+//				stream.write(bytes);
+//				stream.close();
+//
+//				logger.info("Server File Location="
+//						+ serverFile.getAbsolutePath());
+//
+//				return "You successfully uploaded file=" + name;
+//			} catch (Exception e) {
+//				return "You failed to upload " + name + " => " + e.getMessage();
+//			}
+//		} else {
+//			return "You failed to upload " + name
+//					+ " because the file was empty.";
+//		}
+//	}
+
+	/**
+	 * Upload multiple file using Spring Controller
+	 */
+	@RequestMapping(value = "/uploadFiles", method = RequestMethod.POST)
+	public @ResponseBody
+	String uploadMultipleFileHandler(@RequestParam("name") String[] names,
+			@RequestParam("file") MultipartFile[] files) {
+
+		if (files.length != names.length)
+			return "Mandatory information missing";
+
+		String message = "";
+		for (int i = 0; i < files.length; i++) {
+			MultipartFile file = files[i];
+			String name = names[i];
+			try {
+				byte[] bytes = file.getBytes();
+
+				// Creating the directory to store file
+				String rootPath = System.getProperty("catalina.home");
+				File dir = new File(rootPath + File.separator + "tmpFiles");
+				if (!dir.exists())
+					dir.mkdirs();
+
+				// Create the file on server
+				File serverFile = new File(dir.getAbsolutePath()
+						+ File.separator + name);
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(serverFile));
+				stream.write(bytes);
+				stream.close();
+
+				logger.info("Server File Location="
+						+ serverFile.getAbsolutePath());
+
+				message = message + "You successfully uploaded file=" + name
+						+ "<br />";
+			} catch (Exception e) {
+				return "You failed to upload " + name + " => " + e.getMessage();
+			}
+		}
+		return message;
+	}
+}
